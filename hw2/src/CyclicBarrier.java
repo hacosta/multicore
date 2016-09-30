72133593dc07e9b4480ea2244d7a40654faaf040
Hector Acosta <hector.acosta@cloudera.com>
2016-09-29 20:12:10 -0500

    Dump hw2 a
    Signed-off-by: Hector Acosta <hector.acosta@cloudera.com>

---
+++ hw2/src/CyclicBarrier.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.Semaphore;
+
+
+public class CyclicBarrier {
+  int parties;
+  Semaphore sem;
+
+  public CyclicBarrier(int parties) {
+    this.parties = parties;
+    this.sem = new Semaphore(parties, true);
+    try {
+      this.sem.acquire(parties);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public int await() {
+    this.sem.release();
+    return parties - sem.availablePermits();
+  }
+}
---
+++ hw2/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        // Prints "Hello, World" to the terminal window.
+        //System.out.println("Hello, World");
+
+        CyclicBarrier barrier = new CyclicBarrier(4);
+        Party first = new Party(1000, barrier, "PARTY-1");
+        Party second = new Party(2000, barrier, "PARTY-2");
+        Party third = new Party(3000, barrier, "PARTY-3");
+        Party fourth = new Party(4000, barrier, "PARTY-4");
+
+        first.start();
+        second.start();
+        third.start();
+        fourth.start();
+        // do stuff
+
+        //in thread, wait to  stop
+
+    }
+
+}
+
+class Party extends Thread {
+    private int sleeptime;
+    private CyclicBarrier barrier;
+
+    public Party (int time, CyclicBarrier barrier,String name) {
+        super(name);
+        this.sleeptime = time;
+        this.barrier = barrier;
+    }
+    @Override
+    public void run(){
+        try {
+            Thread.sleep(sleeptime);
+            System.out.println(Thread.currentThread().getName() + " is calling await()");
+            int turn = barrier.await();
+            System.out.println(Thread.currentThread().getName() + " got " + turn);
+            System.out.println(Thread.currentThread().getName() + " has started running again");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
