a1a2c77a7b82a5bc7c7418898d8378b25e2bc627
sesamesub <subhash@utexas.edu>
2016-11-03 23:57:25 -0500

    Update LockFreeQueue.java

--- hw3/3/src/LockFreeQueue.java
+++ hw3/3/src/LockFreeQueue.java
@@ -1,98 +1,90 @@
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Random;
 
-public class LockFreeQueue implements MyQueue {
+public class HW3P3 {
 
-    public Node tmp = new Node(0, null);
-    public AtomicReference<Node> head
-            = new AtomicReference<Node>(tmp);
-    public AtomicReference<Node> tail
-            = new AtomicReference<Node>(tmp);
+    final Random random = new Random();
+    final int NUM_THREADS = 20;
 
-    protected class Node {
-
-        public Integer vaule;
-        public AtomicReference<Node> next;
-
-        public Node(int value, Node next) {
-            this.vaule = value;
-            this.next = new AtomicReference<Node>(next);
-        }
-
-        public AtomicReference<Node> getNext (){ return next; }
-        public Integer getValue(){
-            return vaule;
+    public static void sleep(int secs) {
+        try {
+            Thread.sleep(secs * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
-
-    public String printQueue() {
-        String output = "<";
-        //traverse the node till head is null
-        int counter = 0;
-        if (head != null) {
-            //grab the next node
-            Node current = head.get().next.get();
-            //if next node is not null
-            // should take care of exceptions
-            while (current != null) {
-                //print the output
-                output += "[ " ;
-                if (current.getValue() != null) {
-                    output += current.getValue().toString();
-                    }
-                output += " ]";
-                //advance the current node for the next loop
-                current = current.next.get();
-                counter++;
-            }
-        }
-
-        output += ">" + " size of queue: " + counter;
-        return output;
+    public static MyQueue newProtocol(String type) {
+        if(type.equals("LOCK"))
+            return new LockQueue();
+        else
+            return new LockFreeQueue();
     }
 
 
-    public boolean enq(Integer value) {
-        Node newNode = new Node(value, null);
-        while (true) {
-
-            Node currentTail = tail.get();
-            Node tailNext = currentTail.next.get();
-            // verify that current tail has not chagned
-
-            if (currentTail == tail.get()) {
-                if (tailNext != null) {
-                    //  attempt CAS operation, if fail try again
-                    tail.compareAndSet(currentTail, tailNext);
-                } else { // inserting new node
-                    if (currentTail.next.compareAndSet(null, newNode)) {
-                        //  advancing tail
-                        tail.compareAndSet(currentTail, newNode); // if CAS fails, back to start of loop
-                        System.out.println("enq : " + printQueue().toString() );
-                        return true;
-                    }
+    public void parallelTest(final MyQueue MyQueue) throws InterruptedException {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                if(random.nextBoolean()) {
+                    MyQueue.enq(random.nextInt(64));
+                    sleep(random.nextInt(64));
+                } else {
+                    try {
+                        System.out.println("last Head : " + MyQueue.deq().toString());
+                    } catch (NullPointerException ex) {System.out.println("null exception"); }
+                    sleep(random.nextInt(64));
                 }
             }
+        };
+
+        Thread threads[] = new Thread[NUM_THREADS];
+        for (int i = 0; i < NUM_THREADS; i++) {
+            threads[i] = new Thread(r);
+            threads[i].start();
+        }
+        for (int i = 0; i < NUM_THREADS; i++) {
+            threads[i].join();
         }
     }
+    public void simplestTest(MyQueue MyQueue) {
 
-    public Integer deq() {
-        while (true) {
-            Node oldHead = head.get(); // get current head
-            Node oldTail = tail.get(); // get current tail
-            Node oldHeadNext = oldHead.next.get(); // get current head.next
-            if (oldHead == head.get()) { // check old head and tail
-                if (oldHead == oldTail) {
-                    if (oldHeadNext == null)
-                        return null; // there is no blocking , try again
-                    tail.compareAndSet(oldTail, oldHeadNext); // CAS attemp, if fail start loop again
-                } else { // No need to deal with tail
-                    if (head.compareAndSet(oldHead, oldHeadNext))
-                        System.out.println("deq : " + printQueue().toString() );
-                        return oldHeadNext.vaule;
-                        //return null;
-                }
+        /* Simple test. */
+        MyQueue.enq(random.nextInt(64));
+        //myQueue.deq();
+        System.out.println("return value : " + MyQueue.deq().toString() );
+        MyQueue.enq(random.nextInt(64));
+        System.out.println("return value : " + MyQueue.deq().toString() );
+        MyQueue.enq(random.nextInt(64));
+        MyQueue.enq(random.nextInt(64));
+        MyQueue.enq(random.nextInt(64));
+        System.out.println("return value : " + MyQueue.deq().toString() );
+        MyQueue.enq(random.nextInt(64));
+        System.out.println("return value : " + MyQueue.deq().toString() );
+        System.out.println("return value : " + MyQueue.deq().toString() );
+        System.out.println("return value : " + MyQueue.deq().toString() );
+
+    }
+
+    public static void main(String args[]) {
+        HW3P3 hw = new HW3P3();
+
+        //run either test case, it will hang because threads will wait forever for deq in parallel test
+        //String[] protocolTypes = {"LOCK"};
+        String[] protocolTypes = {"LOCK Free"};
+
+
+        for(String type: protocolTypes) {
+            System.out.println("Testing using protocol of type: " + type);
+            try {
+                System.out.println("======================");
+                hw.simplestTest(newProtocol(type));
+                System.out.println("======================");
+                hw.parallelTest(newProtocol(type));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
+
+
     }
 }
